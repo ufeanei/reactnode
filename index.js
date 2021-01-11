eeaea5ce03491d91718a89287f022a14b07fdf5d
ufeanei <akaranjoh@yahoo.com>
2021-01-11 04:16:27 +0100

    passport done 50%

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 // nodejs uses commonjs module system of importing an exporting files
 const express = require('express');
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20')
+const keys = require('./config/keys')
+
 
 const app = express()
+passport.use(
+   new GoogleStrategy({
+     clientID: keys.googleClientID,
+     clientSecret: keys.googleClientSecret,
+     callbackURL: '/auth/google/callback'
+   }, (accessToken,refreshToken, profile, done) => {
+      console.log(accessToken)
+      console.log(profile)
+      console.log(refreshToken)
 
-app.get('/', (req, res) => {
-   res.send({hi: 'there'})
-})
+   })
+
+)
+
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email']}))
+app.get('/auth/google/callback', passport.authenticate('google'))
+
+app.get('/', (req, res) => {res.send('hello')})
+
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=> console.log('hi there'))
