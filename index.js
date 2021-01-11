d23a8aa538ab3bbea03f8f3ff4a2fc8f207ac586
ufeanei <akaranjoh@yahoo.com>
2021-01-11 21:57:33 +0100

    added passport and mongodb

--- index.js
+++ index.js
@@ -1,29 +1,20 @@
-// nodejs uses commonjs module system of importing an exporting files
-const express = require('express');
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth20')
-const keys = require('./config/keys')
 
+const express = require('express'),
+      keys = require('./config/keys'),
+      mongoose = require('mongoose');
+
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true })
+
+require('./models/User') //birng the code of models here above passport since passort uses it 
+require('./services/passport') // bring the code for the passport service here
 
 const app = express()
-passport.use(
-   new GoogleStrategy({
-     clientID: keys.googleClientID,
-     clientSecret: keys.googleClientSecret,
-     callbackURL: '/auth/google/callback'
-   }, (accessToken,refreshToken, profile, done) => {
-      console.log(accessToken)
-      console.log(profile)
-      console.log(refreshToken)
+const authRoutes = require('./routes/authroutes')
 
-   })
 
-)
-
-app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email']}))
-app.get('/auth/google/callback', passport.authenticate('google'))
-
-app.get('/', (req, res) => {res.send('hello')})
+authRoutes(app)
+app.get('/', (req, res) => {res.send('hello je susis la')})
 
 
 
