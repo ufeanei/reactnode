22625271c6e535e03c36a4c05ca0aa6bbb19e60f
ufeanei <akaranjoh@yahoo.com>
2021-01-12 08:45:50 +0100

    prod setting complet

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+if ( process.env.NODE_ENV==='production'){
+    module.exports = require('./dev') 
+}else {
+  module.exports = require('./dev') 
+}
+
+
--- routes/authroutes.js
+++ routes/authroutes.js
@@ -12,3 +12,4 @@ module.exports = (app) =>{
         res.send(req.user)
     })
 }
+
